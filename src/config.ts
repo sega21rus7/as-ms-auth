24a342ac927952db0f817a4756d58dbb283cf6d9
sega21rus7 <sega21rus98@mail.ru>
2021-05-12 18:43:09 +0300

    config depends on env variables

--- src/config.ts
+++ src/config.ts
@@ -1,8 +1,8 @@
 export default () => ({
-  PORT: 3000,
-  POSTGRES_HOST: 'localhost',
-  POSTGRES_PORT: '5432',
-  POSTGRES_USER: 'postgres',
-  POSTGRES_PASSWORD: 'server',
-  POSTGRES_DB: 'as-ms-auth',
+  PORT: process.env.PORT || 3000,
+  POSTGRES_HOST: process.env.POSTGRES_HOST || 'localhost',
+  POSTGRES_PORT: process.env.POSTGRES_PORT || '5432',
+  POSTGRES_USER: process.env.POSTGRES_USER || 'postgres',
+  POSTGRES_PASSWORD: process.env.POSTGRES_PASSWORD || 'server',
+  POSTGRES_DB: process.env.POSTGRES_DB || 'as-ms-auth',
 });
